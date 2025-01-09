52cf1a4a9c26db748d76cb18b9bdefb445b33a3d
Dato <datorogavaa@gmail.com>
2025-01-10 00:12:17 +0400

    commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,51 @@
+import { useState } from "react"
+import styles from "@/styles/Home.module.css"
+export default () => {
+
+    // show/hide useState
+    const [ show, setShow ] = useState(false)
+    // show/hide functions
+    const func = () => {
+        setShow(true)
+
+    }
+    const exit = () => {
+        setShow(false)
+    }
+
+    return (
+            <div>
+            <div>
+                <nav>
+                {
+                show && 
+                    <div className={styles.navbarSecond}>
+                        <br></br>
+                        <div className={styles.navbarTexts}>
+                            <a className={styles.navbarText} href="#">Home</a>
+                            <a className={styles.navbarText} href="#">Products</a>
+                            <a className={styles.navbarText} href="#">About</a>
+                            <a className={styles.navbarText} href="#">Client</a>
+                            <a className={styles.navbarText} href="#">Contact</a>
+                        </div>
+                        <div>
+                            <button className="x" onClick={exit}>X</button>
+                        </div>
+                    </div>
+                }
+                <div className={styles.navbar}>
+                <span ><img onClick={func} src="toggle-icon.png" alt="logo"/></span>
+                    <a  href="index.html"><img src="logo.png"/></a>
+                    <form>
+                        <div>
+                            <a className={styles.navbarA} href="#"><img src="user-icon.png"/></a>
+                            <a className={styles.navbarA} href="#"><img src="bag-icon.png"/></a>
+                            <a className={styles.navbarA} href="#"><img src="search-icon.png"/></a>
+                        </div>
+                    </form>
+                </div>
+                </nav>
+            </div>
+            </div>
+        )
+}
