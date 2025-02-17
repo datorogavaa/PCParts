2c0ffdbfe7f71f23da3aecb4662fc704785435b8
Dato <datorogavaa@gmail.com>
2025-02-17 23:09:56 +0400

    commit

--- src/pages/mainProducts.tsx
+++ src/pages/mainProducts.tsx
@@ -1,36 +1,38 @@
-import { Data } from "@/components/Products"
 import styles from "@/styles/Home.module.css"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 export default () => {
-    const [ amount, setAmount ] = useState(1000)
-    const More = () => {
-        setAmount(amount - 12)
-    }
+    const [products, setProducts ] = useState<any[]>([])
+    useEffect(() => {
+    async function getData() {
+        const response = await fetch('/api/products')
+        const modResponse = await response.json();
+        setProducts(modResponse)
+        }
+        getData();
+    }, [])
+
     return (
         <div>
             <div className={styles.ParentDivofProducts}>
             <h2 className={styles.ProductsHeader}>OUR PRODUCTS</h2>
             {
-            Data.filter((item: any) => item.id > amount && item.price > 0).map(((item: any,key) => {
-                return (
-                <div className={styles.Product} key={key}>
-                    <div className={styles.ProductFirstDiv}>
-                        <h3 className={styles.ProductTitle}>{item.name}</h3>
-                        <h5 className={styles.ProductDescription}>{item.description}</h5>
-                        <img className={styles.ProductImg} src={item.api_featured_image }/>
+                products.map((item: any) => (
+                    <div>
+                        <div className={styles.Product}>
+                            <div className={styles.ProductFirstDiv}>
+                                <h3 className={styles.ProductTitle}>{item.Name}</h3>
+                                <h5 className={styles.ProductDescription}>{item.Description}</h5>
+                                <img className={styles.ProductImg} src='https://sdcdn.io/mac/gb/mac_sku_M0N904_1x1_0.png?width=1080&height=1080'/>
+                            </div>
+                            <div className={styles.ProductSecondDiv}>
+                                <button className={styles.ProductButton}>BUY NOW</button>
+                                <button  className={styles.ProductButton1}>ADD TO CART</button>
+                                <p className={styles.ProductText}>Price <br/> {item.Price}$</p>
+                            </div>
+                        </div>
                     </div>
-                    <div className={styles.ProductSecondDiv}>
-                        <button className={styles.ProductButton}>BUY NOW</button>
-                        <button  className={styles.ProductButton1}>ADD TO CART</button>
-                        <p className={styles.ProductText}>Price <br/> {item.price}$</p>
-                    </div>
-                </div>
-                )
-            }))
+                ))
             }
-            </div>
-            <div style={{ justifyContent: "center", display: 'flex', marginBottom: '40px'}}> 
-                { amount >= 900 &&  <button onClick={More} className={`${styles.seemoreButton}` }>See More</button> }
             </div>
         </div>
     )
